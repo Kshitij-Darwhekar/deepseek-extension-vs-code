565f0229bd6a578f73d6c7da979fa3c573e32497
Kshitij Darwhekar <kshitijdarwhekar@gmail.com>
2025-01-31 18:50:11 +0530

    Updated package.json , few minor changes in extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -16,13 +16,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('deepseek-extension-vs-code.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DeepSeek-Extension-VS-Code!');
-	});
-
-	context.subscriptions.push(disposable);
 
 	const disposable2 = vscode.commands.registerCommand('deepseek-extension-vs-code.start', () => {
 		// The code you place here will be
